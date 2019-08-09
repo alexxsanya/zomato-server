64c6ef3d2977d00097087a596ad4d67a791a7cdd
Alex Ssanya <alexssanya@gmail.com>
2019-08-09 18:29:55 +0300

    update config to pick variables from the env

--- config.js
+++ config.js
@@ -1,17 +1,11 @@
 /**  
-    The config.js 
-    reads the .env file and collects the variables in the envs object.
+    The config.js collects the variables in the envs.
 */
 const dotenv = require('dotenv');
-const result = dotenv.config();
-if (result.error) {
-  throw result.error;
-}
-const { parsed: envs } = result;
 
 module.exports = {
-    API_URL: envs.API_URL,
-    USER_KEY: envs.USER_KEY,
-    PORT: envs.PORT,
-    NODE_ENV: envs.NODE_ENV
+    API_URL: process.env.API_URL,
+    USER_KEY: process.env.USER_KEY,
+    PORT: process.env.PORT,
+    NODE_ENV: process.env.NODE_ENV
 };
